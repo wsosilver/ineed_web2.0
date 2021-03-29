7fcb6e024332eec00e8809a7548e0548f9a6877a
Wenderson Silveira De Oliveira <wso.silver@gmail.com>
2021-03-29 10:45:50 -0300

    ajuste url imagem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl:   'http://ineedapiweb-prod.us-east-2.elasticbeanstalk.com/api',
-  apiUrl: "http://ineedapiweb-prod.us-east-2.elasticbeanstalk.com/",
+  apiUrl: "http://ineedapiweb-prod.us-east-2.elasticbeanstalk.com",
   imageUrl: "http://ineedapiapp-prod.us-east-2.elasticbeanstalk.com",
 };
